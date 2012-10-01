88384d841a24a1b2f5fa179be7fa4a5144381c51
raman <raman@0fd5999a-94d0-4028-93f2-09691c278cb2>
2012-10-01 07:32:53 +0000

    'equals' method

--- src/main/java/eu/trentorise/smartcampus/domain/semantic/Tag.java
+++ src/main/java/eu/trentorise/smartcampus/domain/semantic/Tag.java
@@ -34,4 +34,47 @@ public class Tag implements Serializable {
 	public void setSummary(String summary) {
 		this.summary = summary;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((summary == null) ? 0 : summary.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Tag other = (Tag) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (summary == null) {
+			if (other.summary != null)
+				return false;
+		} else if (!summary.equals(other.summary))
+			return false;
+		return true;
+	}
+	
 }
